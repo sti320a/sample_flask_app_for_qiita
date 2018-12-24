ad840b582873cdf4b71f51e7b2c675df56c89e3e
sti320a <sti320a@gmail.com>
2018-12-24 14:13:45 +0900

    Add for on template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-# render_templateを追加
 from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
 def index():
-    # render_templateでindex.htmlを読み込んで、表示する
-    return render_template('index.html') 
+    # ユーザー名をリストusersに格納する
+    users = ['Flask太郎', 'Python一郎', 'Jinja次郎']
+    # render_templateの第2引数に、users=usersを追加
+    return render_template('index.html', users=users) 
 
 if __name__ == "__main__":
     app.run(debug=True)
