cafe790a94b532ce79e0a3abdca11d3825ed1e8d
Tomoaki Sugiayma <sti320a@gmail.com>
2019-01-07 22:53:02 +0900

    Updated db

--- model.py
+++ model.py
@@ -1,19 +1,16 @@
-import sqlite3 # 標準ライブラリであるsqlite3をインポートする
+import sqlite3
 
-connection = sqlite3.connect('./db/sample.db') # データベースファイルにアクセスする
-cursor = connection.cursor() #　Cursorオブジェクトをつくる（いまは理解しなくても大丈夫）
+connection = sqlite3.connect('./db/sample.db')
+cursor = connection.cursor()
 
-# codeという名前のテーブルを作成する
 cursor.execute("CREATE TABLE IF NOT EXISTS code (id int, contents varchar(1024))")
 
-# codeテーブルにコードを保存する
 cursor.execute("INSERT INTO code(id, contents) VALUES(?,?)", [1, "print('Hello, world')"])
-conn.commit() # 保存をコミット
+connection.commit()
 
-# codeテーブルの全データを取得する
 result = cursor.execute("SELECT * FROM code")
 
 for row in result:
     print(row)
 
-conn.close()　# 接続を切る（理解しなくてもよいが、必ず書く）
+connection.close()
