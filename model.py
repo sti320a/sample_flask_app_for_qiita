790d45761064e71184548253ddccf3b4d37bcb20
Tomoaki Sugiayma <sti320a@gmail.com>
2019-01-07 22:45:47 +0900

    Create db folder

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
-import sqlite3
+import sqlite3 # 標準ライブラリであるsqlite3をインポートする
 
-connection = sqlite3.connect('./db/sample.db')
-cursor = connection.cursor()
+connection = sqlite3.connect('./db/sample.db') # データベースファイルにアクセスする
+cursor = connection.cursor() #　Cursorオブジェクトをつくる（いまは理解しなくても大丈夫）
 
+# codeという名前のテーブルを作成する
 cursor.execute("CREATE TABLE IF NOT EXISTS code (id int, contents varchar(1024))")
-cursor.execute("INSERT code (id int, contents varchar(1024))")
-cursor.execute("SELECT * FROM code")
 
-conn.commit()
-conn.close()
+# codeテーブルにコードを保存する
+cursor.execute("INSERT INTO code(id, contents) VALUES(?,?)", [1, "print('Hello, world')"])
+conn.commit() # 保存をコミット
+
+# codeテーブルの全データを取得する
+result = cursor.execute("SELECT * FROM code")
+
+for row in result:
+    print(row)
+
+conn.close()　# 接続を切る（理解しなくてもよいが、必ず書く）
